8f82f0c14ffadfa26cefe931ecb993c3e1578cda
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-04-19 10:08:30 +0300

    Partial created for the recipe block

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -1,4 +1,5 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
+import { recipeBlock } from '../partials/recipeBlock.js';
 
 export const homeTemplate = (recipes) => html`
     <section id="home">
@@ -7,25 +8,7 @@ export const homeTemplate = (recipes) => html`
         ${recipes.length !== 0 ? html`
             <h3>Latest recipes added are...</h3>
                 ${recipes.map((recipe) => html`
-                <div class="recipe-block">
-                    <div class="recipe-actions">
-                        <a href="/recipes/edit/${recipe._id}">
-                            <span class="material-symbols-outlined">
-                                edit_square
-                            </span>
-                        </a>
-                        <a href="/recipes/delete/${recipe._id}">
-                            <span class="material-symbols-outlined">
-                                delete
-                            </span>
-                        </a>
-                    </div>
-                    <div class="recipe-content">
-                        <h1>Recipe: ${recipe.title}</h1>
-                        <p>How to: ${recipe.description}</p>
-                        <p>Author: ${recipe.author}</p>
-                    </div>
-                </div>
+                    ${recipeBlock(recipe)}
                 `)}
         ` : html`
             <h2>There are no recipes :(</h2>
