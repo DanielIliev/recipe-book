b3db0f5c7cb387956061290ea60231014a75e186
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-05-15 21:54:14 +0300

    Update

--- src/views/layoutView.js
+++ src/views/layoutView.js
@@ -6,13 +6,22 @@ export const layoutView = (content) => html`
         <a href="/" id="brand">${localeEnglish.navigation.logoLinkText}</a>
         <ul>
             <li>
-                <a href="/add">${localeEnglish.navigation.addLinkText}</a>
+                <a class="desktop-links" href="/add">${localeEnglish.navigation.addLinkText}</a>
+                <a class="mobile-links" href="/add">
+                    <i class="ph-light ph-plus"></i>
+                </a>
             </li>
             <li>
-                <a href="/recipes">${localeEnglish.navigation.recipesLinkText}</a>
+                <a class="desktop-links" href="/recipes">${localeEnglish.navigation.recipesLinkText}</a>
+                <a class="mobile-links" href="/recipes">
+                    <i class="ph-light ph-notebook"></i>
+                </a>
             </li>
             <li>
-                <a href="/about">${localeEnglish.navigation.aboutLinkText}</a>
+                <a class="desktop-links" href="/about">${localeEnglish.navigation.aboutLinkText}</a>
+                <a class="mobile-links" href="/about">
+                    <i class="ph-light ph-info"></i>
+                </a>
             </li>
         </ul>
     </nav>
